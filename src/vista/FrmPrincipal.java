99e3c2a5720fa3c988cdd9342abb96f14c5ff66a
JULIAN HUAMANI <jhqhuamani@gmail.com>
2019-11-10 00:15:38 -0500

    Gui del fomulario 0.1

--- src/vista/FrmPrincipal.java
+++ src/vista/FrmPrincipal.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class FrmPrincipal extends JFrame {
 
@@ -42,11 +43,26 @@ public class FrmPrincipal extends JFrame {
 		JMenu mnNewMenu = new JMenu("Archivo");
 		menuBar.add(mnNewMenu);
 		
-		JMenu mnNewMenu_2 = new JMenu("Perfil");
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Cerrar");
+		mnNewMenu.add(mntmNewMenuItem_3);
+		
+		JMenu mnNewMenu_2 = new JMenu("Matricula");
 		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Registrar");
+		mnNewMenu_2.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Matenimiento");
+		mnNewMenu_2.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Consulta");
+		mnNewMenu_2.add(mntmNewMenuItem_2);
 		
 		JMenu mnNewMenu_1 = new JMenu("Ayuda");
 		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Acerca del Sistema");
+		mnNewMenu_1.add(mntmNewMenuItem_4);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
---
+++ src/vista/FrmRegistraMatricula.java
@@ -0,0 +1,58 @@
+package vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class FrmRegistraMatricula extends JInternalFrame {
+	private JTextField txtDni;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmRegistraMatricula frame = new FrmRegistraMatricula();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmRegistraMatricula() {
+		setIconifiable(true);
+		setClosable(true);
+		setMaximizable(true);
+		setBounds(100, 100, 651, 470);
+		getContentPane().setLayout(null);
+		
+		txtDni = new JTextField();
+		txtDni.setBounds(81, 46, 96, 20);
+		getContentPane().add(txtDni);
+		txtDni.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("DNI");
+		lblNewLabel.setBounds(34, 49, 48, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNombres = new JLabel("Nombres");
+		lblNombres.setBounds(34, 82, 48, 14);
+		getContentPane().add(lblNombres);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(81, 79, 96, 20);
+		getContentPane().add(textField);
+
+	}
+}
